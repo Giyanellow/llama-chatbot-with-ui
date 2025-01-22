20561f37a17b2dc780f1d54f995904d03a01ab0d
Giyanellow <montenegroblends@gmail.com>
2025-01-23 00:07:13 +0800

    refactor: update system prompt for chatbot and enhance message handling

--- backend/utils/chat.py
+++ backend/utils/chat.py
@@ -15,6 +15,20 @@ sync_connection = psycopg.connect(conn_info)
 
 chat_history_table = "chat_history"
 
+system_prompt = """
+    You are an assistant that helps users with understanding Harry Potter books. 
+    Your main task is to answer any questions that the user may have regarding Harry Potter and its universe.
+
+    Reminders:
+        Always remember to be kind, respectful and enthusiastic with your messages.
+        Never communicate negative or harmful messages.
+    
+    DO NOT answer any questions that are not related to Harry Potter or its universe.
+    
+    Output:
+        Always format your output in proper markdown format.
+"""
+
 class ChatBot:
     def __init__(self, session_id):
         self.llm = llm
@@ -27,14 +41,15 @@ class ChatBot:
             sync_connection=sync_connection
         )
         
-        # Add some messages to the chat history
-        self.chat_history.add_messages([
-            SystemMessage(content="System message example"),
-            AIMessage(content="AI message example"),
-            HumanMessage(content="Human message example"),
-        ])
+    def get_message_history(self):
+        """gets the message history from the chat history
         
-        print(self.chat_history.get_messages())
+        Returns:
+            list: list of messages
+        """
+        
+        return self.chat_history.get_messages()
+        
     
     def invoke(self, text: str) -> str:
         """invokes the model with the given text
@@ -46,7 +61,11 @@ class ChatBot:
             str: response from the model
         """
         
-        response = self.llm.invoke(input=text)
+        messages = self.chat_history.get_messages()
+        messages.append(SystemMessage(content=system_prompt))
+        messages.append(HumanMessage(content=text))
+        
+        response = self.llm.invoke(input=messages)
         
         return response.content
     
@@ -60,7 +79,11 @@ class ChatBot:
             str: streams the response from the model
         """
         
-        for chunk in self.llm.stream(input=text):
+        messages = self.chat_history.get_messages()
+        messages.append(SystemMessage(content=system_prompt))
+        messages.append(HumanMessage(content=text))
+        
+        for chunk in self.llm.stream(input=messages):
             yield chunk.content
     
     
